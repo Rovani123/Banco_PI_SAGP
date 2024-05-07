d78a52bf2aa33fe718f8be2f064afad188db63a9
danielar03 <145597021+danielar03@users.noreply.github.com>
2024-05-07 11:18:00 -0300

    Create delete.sql

---
+++ delete.sql
@@ -0,0 +1,13 @@
+Delete from funcionarios where id_funcionario = ?;
+
+Delete from produtos where id_produto = ?;
+
+Delete from vendas where id_venda = ?;
+
+Delete from carrinho where id_carrinho = ?;
+
+Delete from clientes where id_cliente = ?;
+
+Delete from promocoes where id_promocoes = ?;
+
+Delete from produtos_promocao where id_produto = ?;
