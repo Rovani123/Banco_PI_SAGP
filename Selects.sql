f60cc4acc094b8ac6f0ae6d406f81e4070be1e3e
Jesus <118839372+Rovani123@users.noreply.github.com>
2024-05-13 11:07:46 -0300

    Update Selects.sql

--- Selects.sql
+++ Selects.sql
@@ -4,15 +4,8 @@ select * from funcionarios where usuario_funcionario = ? and senha = ?;
 select * from produtos;
 
 select * from vendas;
-select * from vendas as v 
-inner join funcionario as f on f.id_funcionario = v.id_funcionario
-inner join cliente as c on c.id_cliente = v.id_cliente;
 
 select * from carrinho;
-select * from carrinho as c 
-inner join produto as p on p.id_produto = c.id_produto
-inner join vendas as v on v.id_venda = c.id_venda;
-
 
 select * from clientes;
 select * from clientes where nome_cliente = ? and cpf_cliente = ?;
@@ -20,7 +13,8 @@ select * from clientes where nome_cliente = ? and cpf_cliente = ?;
 select * from promocoes;
 
 
-select * from produtos_promocao;
-select * from produtos_promocao as pp 
-inner join produto as p on p.id_produto = pp.id_produto
-inner join promocao as pr on pr.id_promocao = pp.id_cliente;
+select * from carrinho as c 
+inner join vendas as v on v.id_venda = c.id_venda
+inner join produtos as p on c.id_produto = p.id_produto
+inner join funcionarios as f on f.id_funcionario = v.id_funcionario
+inner join clientes as cli on cli.id_cliente = v.id_cliente;
