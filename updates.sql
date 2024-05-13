578c6ba1c820fc35d1c8fb30ac662398dd52dd72
danielar03 <145597021+danielar03@users.noreply.github.com>
2024-05-13 08:54:09 -0300

    Update updates.sql

--- updates.sql
+++ updates.sql
@@ -1,2 +1,15 @@
 UPDATE nome_tabela SET coluna1 = valor1, coluna2= valor2 WHERE condicao;
+UPDATE ? SET coluna1 = valor1, coluna2= valor2 WHERE ?;
+
+UPDATE funcionario SET coluna1 = valor1, coluna2= valor2 WHERE condicao;
+
+UPDATE produto SET coluna1 = valor1, coluna2= valor2 WHERE condicao;  
+
+UPDATE cliente SET coluna1 = valor1, coluna2= valor2 WHERE condicao; 
+
+UPDATE vendas SET coluna1 = valor1, coluna2= valor2 WHERE condicao;
+
+UPDATE promocoes SET coluna1 = valor1, coluna2= valor2 WHERE condicao;
+
+UPDATE produto_promocoes SET coluna1 = valor1, coluna2= valor2 WHERE condicao;
 
