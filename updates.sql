775f21edbf56d79f2fd6689cd7e4b2f242640c62
lezio09 <144951527+lezio09@users.noreply.github.com>
2024-05-13 09:09:34 -0300

    update de funcionarios e produtos

--- updates.sql
+++ updates.sql
@@ -1,15 +1,18 @@
 UPDATE nome_tabela SET coluna1 = valor1, coluna2= valor2 WHERE condicao;
-UPDATE ? SET coluna1 = valor1, coluna2= valor2 WHERE ?;
+UPDATE ? SET coluna1 = valor1, coluna2= valor2 WHERE IS condicao;
 
-UPDATE funcionario SET coluna1 = valor1, coluna2= valor2 WHERE condicao;
+UPDATE funcionario SET nome_funcionario = ?, usuario_funcionario= ?, senha = ? WHERE IS ?;
+UPDATE funcionario SET nome_funcionario = 'Lezio', usuario_funcionario= 'Lezion', senha = 'funcpadaria' WHERE IS ?;
 
-UPDATE produto SET coluna1 = valor1, coluna2= valor2 WHERE condicao;  
+UPDATE produto SET nome_produto = ?, preco= ?, quantidade_estoque = ?, categoria = ?  WHERE IS ?;
+UPDATE produto SET nome_produto = 'pastel de carne', preco= 7.99, quantidade_estoque = 6, categoria = 'salgado'  WHERE IS ?;
 
-UPDATE cliente SET coluna1 = valor1, coluna2= valor2 WHERE condicao; 
 
-UPDATE vendas SET coluna1 = valor1, coluna2= valor2 WHERE condicao;
+UPDATE cliente SET coluna1 = valor1, coluna2= valor2 WHERE IS condicao; 
 
-UPDATE promocoes SET coluna1 = valor1, coluna2= valor2 WHERE condicao;
+UPDATE vendas SET coluna1 = valor1, coluna2= valor2 WHERE IS condicao;
 
-UPDATE produto_promocoes SET coluna1 = valor1, coluna2= valor2 WHERE condicao;
+UPDATE promocoes SET coluna1 = valor1, coluna2= valor2 WHERE IS condicao;
+
+UPDATE produto_promocoes SET coluna1 = valor1, coluna2= valor2 WHERE IS condicao;
 
