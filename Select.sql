8353b16ef320228e1c2a406e777b0bbf3c7cb7a8
LucasIValle <lucas.irvalle@gmail.com>
2024-05-13 08:59:01 -0300

    select where

--- Select.sql
+++ Select.sql
@@ -1,17 +1,16 @@
 select * from funcionarios;
+select * from funcionarios where usuario_funcionario = ? and senha = ?;
 
 select * from produtos;
-select ? from produtos where ?;
 
 select * from vendas;
-select ? from vendas ?;
 
 select * from carrinho;
-select ? from carrinho where ?;
 
 select * from clientes;
+select * from clientes where nome_cliente = ? and cpf_cliente = ?;
+
 
 select * from promocoes;
 
 select * from produtos_promocao;
-select ? from produtos_promocao ?;
